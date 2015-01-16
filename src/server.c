97558d1d3843c30fae44bf3043f0725d95a2b34b
Giuliano Peraz <giuliano.peraz@gmail.com>
2015-01-16 17:06:30 +0100

    Added the spine of the server. Well, just a vertebra.

---
+++ src/server.c
@@ -0,0 +1,15 @@
+/**
+ * David Costa <david.costa@ieee.org>, Giuliano Peraz <giuliano.peraz@gmail.com>
+ */
+
+/**
+ * The server will open a socket and waits a client connection request. At the first
+ * message the protocol is started. The server must wait until the socket is over.
+ */
+
+#include <common.h>
+
+int main (void)
+{
+	return 0;
+}
