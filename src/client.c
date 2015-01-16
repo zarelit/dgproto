ae75d853c270d92d20de8f12107f3c5c51b54e31
Giuliano Peraz <giuliano.peraz@gmail.com>
2015-01-16 17:01:35 +0100

    Added some lines, just makes sense on this commit (English master uh?)

---
+++ src/client.c
@@ -0,0 +1,19 @@
+/**
+ * David Costa <david.costa@ieee.org>, Giuliano Peraz <giuliano.peraz@gmail.com>
+ */
+
+
+/**
+ * This is the client implementation for the D&G® Secure Protocol.
+ * The client simply opens a socket to a server, of which has the public key, and then
+ * start the protocol in order to establish a session key for sending something
+ * through the unsecure channel.
+ */
+
+#include <common.h>
+
+
+int main (void)
+{
+	return 0;
+}
