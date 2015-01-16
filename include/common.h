bd37e61045775364a352a66fb40f0fd432d2af24
Giuliano Peraz <giuliano.peraz@gmail.com>
2015-01-16 16:50:32 +0100

    Some really unuseful things. Like includes, for example.

---
+++ include/common.h
@@ -0,0 +1,17 @@
+/**
+ * David Costa <david.costa@ieee.org>, Giuliano Peraz <giuliano.peraz@gmail.com>
+ */
+
+// Guard directives
+#ifndef CLIENT_H
+#define CLIENT_H 1
+
+#include <stdio.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <openssl/evp.h>
+
+#endif
