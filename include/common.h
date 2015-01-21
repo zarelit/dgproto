e2b73a3e2fd0a9f8b85c7774774ac463acee1c65
Giuliano Peraz <giuliano.peraz@gmail.com>
2015-01-21 11:14:51 +0100

    Deleted unuseful guard commands
    Only one file at a time will access to only one header. So why do we
    need guard commands?

--- include/common.h
+++ include/common.h
@@ -2,17 +2,11 @@
  * David Costa <david.costa@ieee.org>, Giuliano Peraz <giuliano.peraz@gmail.com>
  */
 
-// Guard directives
-#ifndef CLIENT_H
-#define CLIENT_H 1
-
 #include <stdio.h>
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <inttypes.h>
-#include <arpa/inet.h>
+#include <arpa/inet.h> // For hton* and ntoh* byte conversion functions
 #include <netinet/in.h>
 #include <openssl/evp.h>
-
-#endif
