ff71fcbf427df7b9c180520b5a8ba989b0471589
David Costa <david@zarel.net>
2015-01-22 13:42:41 +0100

    Client resolves server name

--- src/client.c
+++ src/client.c
@@ -11,13 +11,32 @@
  */
 
 #include "../include/common.h"
+#include <string.h>
 
 
 int main (int argc, char** argv)
 {
+	struct addrinfo query;
+	struct addrinfo *servers;
+
+	int s; // exit status of some calls, used for error checking.
+
 	if(argc != 3){
 		printf("Usage: %s <Server IPv4> <file to send>\n",argv[0]);
 		exit(EXIT_FAILURE);
 	}
+
+	// Ask the system to resolv the name if needed and build a valid
+	// addrinfo in order to connect.
+	memset(&query, 0, sizeof query);	// Erase unneeded fields
+	query.ai_family = AF_INET;			// IPv4 only
+	query.ai_socktype = SOCK_STREAM;	// Protocol runs on TCP
+
+	s = getaddrinfo(argv[1], SRV_PORT, &query, &servers);
+	if(s){
+		fprintf(stderr,"Cannot connect to server: %s\n",gai_strerror(s));
+		exit(EXIT_FAILURE);
+	}
+
 	return EXIT_SUCCESS;
 }
