c26ddf331b2528b3d939a6d763692e0d7f09556c
David Costa <david@zarel.net>
2015-01-25 16:06:15 +0100

    make functions respect the header signatures

--- src/protocol.c
+++ src/protocol.c
@@ -10,28 +10,28 @@
 
 #include "../include/protocol.h"
 
-uint8_t* create_m1 (uint64_t *msg_len, uint8_t id, BIGNUM** Na)
+uint8_t* create_m1 (uint64_t *msg_len, uint8_t id, BIGNUM* Na)
 {
     uint8_t* msg;
 
     return msg;
 }
 
-uint8_t* create_m2 (uint64_t *msg_len, uint8_t id, BIGNUM** Nb)
+uint8_t* create_m2 (uint64_t *msg_len, uint8_t id, BIGNUM* Nb)
 {
     uint8_t* msg;
 
     return msg;
 }
 
-uint8_t* create_m3 (uint64_t *msg_len, BIGNUM* key, BIGNUM** Nb)
+uint8_t* create_m3 (uint64_t *msg_len, BIGNUM* key, BIGNUM* Nb)
 {
     uint8_t* msg;
 
     return msg;
 }
 
-uint8_t* create_m4 (uint64_t *msg_len, BIGNUM* key, BIGNUM** Nb)
+uint8_t* create_m4 (uint64_t *msg_len, BIGNUM* key, BIGNUM* Nb)
 {
     uint8_t* msg;
 
