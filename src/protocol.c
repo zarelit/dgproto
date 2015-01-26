126e8aebb6bd78484cbe0063130e644e33564dca
David Costa <david@zarel.net>
2015-01-26 20:13:38 +0100

    Sign nonce Na in M1

--- src/protocol.c
+++ src/protocol.c
@@ -25,11 +25,22 @@ create_m1 (uint64_t *msg_len, uint8_t id, BIGNUM* Na)
 	FILE* skeyfh;
 
 
+	// Signing related variables
+	EVP_PKEY_CTX* sigctx; // context
+	unsigned char* sig; // actual signature
+	size_t siglen; //length of the signature
+	unsigned char* Na_value; // Bytes of Na in big-endian format
+	size_t Na_size; // Length of Na in bytes
+
+
+	// Encryption related variables
+	EVP_PKEY_CTX* encctx;
+
 	// Load client key, called client.pem
 	ckeyfh = fopen("keys/client.pem","r");
 	ckey = PEM_read_PrivateKey(ckeyfh, &ckey, NULL, NULL);
 	if(!ckey){
-		fprintf(stderr,"Cannot read client key from file %s\n");
+		fprintf(stderr,"Cannot read client key from file keys/client.pem\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -37,11 +48,57 @@ create_m1 (uint64_t *msg_len, uint8_t id, BIGNUM* Na)
 	skeyfh = fopen("keys/server.pem","r");
 	skey = PEM_read_PUBKEY(skeyfh, &skey, NULL, NULL);
 	if(!skey){
-		fprintf(stderr,"Cannot read server key from file %s\n");
+		fprintf(stderr,"Cannot read server key from file keys/server.pem\n");
 		exit(EXIT_FAILURE);
 	}
 
+	/*
+	 * Step 0: Convert Na in a signable buffer
+	 */
+	Na_size = BN_num_bytes(Na);
+	Na_value = malloc(Na_size);	
+	BN_bn2bin(Na, Na_value); 
 
+	/*
+	 * Step 1: Sign Na
+	 */
+	// create signing context 
+	sigctx = EVP_PKEY_CTX_new(ckey, NULL);
+	if (!sigctx){
+		fprintf(stderr,"Cannot create a signing context\n");
+		exit(EXIT_FAILURE);
+	}
+	if (EVP_PKEY_sign_init(sigctx) <= 0){
+		fprintf(stderr,"Cannot create a signing context\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Ask the maximum signature size to OpenSSL when signing
+	// a quantity like Na
+	if (EVP_PKEY_sign(ctx, NULL, &siglen, Na_value, Na_size ) <= 0)
+		exit(EXIT_FAILURE);
+
+	sig = malloc(siglen);
+	if(!sig){
+		fprintf(stderr,"Out of memory\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Do the real signature
+	if (EVP_PKEY_sign(sigctx, sig, &siglen, Na_value, Na_size) <= 0){
+		fprintf(stderr,"Cannot sign nonce Na\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/*
+	 * Step 2: Encrypt the signed Na using the server public key
+	 */
+
+	/*
+	 * Cleanup
+	 */
+	free(sig);
+	free(Na_value);
     return msg;
 }
 
