af5ec4ae44fca1c54cccb81de33f98b68757e47e
Giuliano Peraz <giuliano.peraz@gmail.com>
2015-01-27 12:59:25 +0100

    Added read_iv_from_file

--- src/utils.c
+++ src/utils.c
@@ -31,10 +31,43 @@ void hexdump(FILE *fh, unsigned char* buf, size_t buflen){
 	}
 }
 
+/**
+ * This function reads the Initialization Vector (IV) from the file path passed by parameter.
+ * \param iv_file the Initialization Vector for the cipher.
+ * \returns a buffer that contains the IV or NULL if an error has occourred.
+ */
+uint8_t*
+read_iv_from_file (FILE* iv_file)
+{
+    uint8_t *iv_buf;
+    size_t byte_read;
+
+    iv_buf = malloc(EVP_MAX_IV_LENGTH);
+    byte_read = fread((void*) iv_buf, sizeof(uint8_t), EVP_MAX_IV_LENGTH, iv_file);
+    if(byte_read < EVP_MAX_IV_LENGTH * sizeof(uint8_t))
+    {
+        fprintf(stderr, "Can't read the whole IV\n");
+        if(feof(iv_file) > 0)
+        {
+            fprintf(stderr, "EOF reached before expected\n");
+        }
+        else if (ferror > 0)
+        {
+            fprintf(stderr, "Error reading the file\n");
+        }
+        free(iv_buf);
+    }
+    return iv_buf;
+}
+
 uint8_t*
 do_aes256_crypt (uint8_t* msg, uint8_t* key, uint64_t* msg_len)
 {
-    
+    EVP_CIPHER_CTX *ctx;
+    uint8_t *encr_msg;
+    size_t enc_len, bsize; // Encypted message length and block size of the cipher
+
+    EVP_CIPHER_CTX_Init(ctx);
 }
 
 uint8_t*
