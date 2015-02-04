6e28d3063ff8af0145ab925bf7997c36949963cb
David Costa <david@zarel.net>
2015-02-04 17:57:17 +0100

    spostate alcune macro in utils.h

--- test/util_tests.c
+++ test/util_tests.c
@@ -2,13 +2,6 @@
 #include "../include/utils.h"
 #include "../include/protocol.h"
 
-#define say(yeah) printf("%s\n",yeah)
-#define dump(name, quantity, length) printf("%s length: %d\n", name, length);\
-									 printf("%s dump:\n", name);\
-									 hexdump(stdout,quantity, length);\
-									 printf("\n");
-#define doing(something) printf("** "); say(something);
-
 int
 test_conc_msgs (void)
 {
