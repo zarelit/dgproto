aa0ea4e6395286ce2ace9e70606c621baabb0406
David Costa <david@zarel.net>
2015-02-09 18:53:46 +0100

    Some functions

--- test/gen_dialog.sh
+++ test/gen_dialog.sh
@@ -9,6 +9,41 @@ then
 	exit 1
 fi
 
+# gen_nonce 16 Na.bin -> puts 16 bytes in Na.bin
+function gen_nonce {
+	openssl rand -out "$2" "$1"
+}
+
+function sign {
+	openssl rsautl -sign -in "$2" -inkey "$1" -out "sign$2"
+}
+
+function envelope {
+	# IV
+	gen_nonce 16 "IV_${1}.bin"
+	# Naked EK
+	gen_nonce 32 "EphKey_${1}.bin"
+	# Protected EK
+	openssl rsautl -encrypt -in "EphKey_${1}.bin" -pubin -inkey "$2" -out "EK_${1}.bin"
+	# Symmetrically encrypted payload
+}
+
+function createM1 {
+	echo "** Generate M1"
+	echo "Generate Na"
+	gen_nonce 16 Na.bin
+	echo "Sign Na"
+	sign "$CLIENT" Na.bin 
+	echo "Concatenate them"
+	cat Na.bin signNa.bin > Na+sign.bin
+	echo "Generate envelope: IV, EK, C"
+	envelope "M1" "$PUBSERVER" Na+sign.bin
+}
+
+function file2hex {
+	hexdump -ve '1/1 "%.2x"' $1
+}
+
 # Transform in absolute paths
 pushd "$1" > /dev/null
 CLIENT="$PWD/client.pem"
@@ -20,17 +55,3 @@ popd
 # Move to the working directory
 pushd "$2" > /dev/null
 
-# Generate M1
-echo "Generate nonces"
-openssl rand -out Na.bin 16
-openssl rand -out Nb.bin 16
-
-echo "Sign nonces"
-openssl rsautl -sign -in Na.bin -inkey "$CLIENT" -out signNa.bin
-openssl rsautl -sign -in Nb.bin -inkey "$SERVER" -out signNb.bin
-
-echo "Generate N || sign(N)"
-cat Na.bin signNa.bin > Na+sign.bin
-cat Nb.bin signNb.bin > Nb+sign.bin
-
-echo "Generate encrypted part of M1"
