50321fff9342434aa6dd64e7d28894933230d756
David Costa <david@zarel.net>
2015-02-10 12:19:50 +0100

    test protocollo: genera M1 e verifica corretto M1

--- test/proto_tests.c
+++ test/proto_tests.c
@@ -3,14 +3,35 @@
 #include "../include/utils.h"
 
 int main(){
-	BIGNUM* Na;
-	uint8_t* m1;
-	size_t m1_len;
 
+	BIGNUM* Na;
+	BIGNUM* Na_ofB;
+	msg_data m1;
+
+	int test;
+
+	doing("Client generates Na");
 	Na = generate_random_nonce();
-	m1 = create_m1 (&m1_len, 'A', Na);
-	if( m1 == NULL) exit(EXIT_FAILURE);
-	dump("M1",m1,m1_len);	
+
+	doing("Client sends M1");
+	m1.data = create_m1 (&(m1.data_len), 'A', Na);
+	if( m1.data == NULL){
+		say("1. Test failed. Cannot create M1");
+		exit(EXIT_FAILURE);
+	}else{
+		say("1. Test ok. M1 generated succesfully");
+		//dump("M1",m1.data,m1.data_len);	
+	}
+
+	say("");
+	doing("Server verifies M1 and extracts Na from it");
+	test = verifymessage_m1 (m1.data, m1.data_len, &Na_ofB);
+	if(test != 1){
+		say("2. Test failed. M1 is not verified correctly");
+		exit(EXIT_FAILURE);
+	} else {
+		say("2. Test ok. M1 is verified correctly");
+	}
 
 	return 0;
 }
